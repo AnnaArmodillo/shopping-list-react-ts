6242733a44d0b984c19b1c991b3182573f99e7cb
Anna Denisova <abachledacurus@gmail.com>
2023-04-06 18:39:27 +0700

    Реализовано появление/исчезновение формы по клику

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,26 @@
 import { NewItemForm } from 'components/NewItemForm/NewItemForm';
+import { FC, useState } from 'react';
+import styles from './header.module.scss';
 
-export const Header = () => {
+export const Header: FC = () => {
+  const [isActive, setIsActive] = useState(false);
   return (
     <>
-      <h4>Запланировать новую покупку</h4>
-      <NewItemForm />
+      <h4>
+        Запланировать новую покупку{' '}
+        {isActive ? (
+          <button type="button" className={styles.button} onClick={() => setIsActive(!isActive)}>
+            <i className="fa-solid fa-chevron-up" />
+          </button>
+        ) : (
+          <button type="button" className={styles.button} onClick={() => setIsActive(!isActive)}>
+            <i className="fa-solid fa-chevron-down" />
+          </button>
+        )}
+      </h4>
+      <div className={isActive ? '' : styles.hidden}>
+        <NewItemForm />
+      </div>
     </>
   );
 };
